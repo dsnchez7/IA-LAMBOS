1f8e553eb04de3b51e1db42c5b4887ce7656c186
dsnchez7 <dcalvio48@gmail.com>
2025-04-24 16:31:47 -0600

    Update index.js

--- index.js
+++ index.js
@@ -9,12 +9,11 @@ app.get('/screenshot', async (req, res) => {
   if (!url) return res.status(400).send('Missing TradingView URL.');
 
   const browser = await puppeteer.launch({
-    args: ['--no-sandbox'],
-    headless: 'new' // Usar nuevo modo compatible con entornos serverless
+    headless: true,
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
   });
 
   const page = await browser.newPage();
-
   await page.goto(url, { waitUntil: 'networkidle2' });
   await page.setViewport({ width: 1280, height: 720 });
 
@@ -25,7 +24,7 @@ app.get('/screenshot', async (req, res) => {
   res.send(screenshot);
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Servidor escuchando en puerto ${PORT}`);
 });
